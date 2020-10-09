bb46563eeb1ef1c4f77684ca1c7091ae6b6e3a5e
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-10-09 11:53:09 +0100

    Basic Version Complete
    The score comparion to the last elections has been changed to be much more lenient and now seems to pick appropriate candidates.

--- CandidatesDecider.py
+++ CandidatesDecider.py
@@ -23,8 +23,17 @@ totalDown = 0
 
 array = []
 def inputArray():
+	global totalUp
+	global totalDown
+	array = []
 	name = ""
-	name = input("Enter name of candidate")
+	name = input("Enter name of candidate\nEnter 'import' to import saved array")
+	if name == "import":
+		array = importArray()
+		for x in range(len(array)):
+			totalUp += array[x][1]
+			totalDown += array[x][2]
+		return array
 	while name != "":
 		up = int(input("Input up-votes"))
 		totalUp += up
@@ -32,11 +41,12 @@ def inputArray():
 		totalDown += down
 		array.append([name,up,down])
 		name = input("Enter name of candidate (leave blank to stop)")
+	return array
 
 def exportArray():
 	arrayFile = open("candidates.txt","w")
 	for x in range(len(array)):
-		arrayFile.write(array[x]+"\n")
+		arrayFile.write(array[x][0]+","+str(array[x][1])+","+str(array[x][2])+"\n")
 	arrayFile.close()
 
 def importArray():
@@ -45,20 +55,33 @@ def importArray():
 	except FileNotFoundError:
 		return []
 	array = []
-	for x in range(file_len(arrayFile)):
+	for x in range(file_len("candidates.txt")):
 		line = arrayFile.readline()
 		array.append(line.split(","))
+		array[x][1] = int(array[x][1])
+		array[x][2] = int(array[x][2])
 	arrayFile.close()
 	return array
 
+array = inputArray()
 candidateNo = len(array)
 votesPerMember = (totalUp+totalDown)/members
 averageUp = totalUp/candidateNo
 averageDown = totalDown/candidateNo
+print("Election Results:")
 for x in range(candidateNo):
 	if array[x][1] > averageUp: #Upvotes must be more than average
 		if array[x][1] > array[x][2]*2: #Must have at least 2X more upvotes than downvotes
-			if array[x][2] < totalDown/2: #Candidates has less than half the total amount of downvotes
-				if (array[x][1]-array[x][2])/members > (0.7/10): #Checks their score is high enough compared to the total votes from the last polls
+			if array[x][2] < round(totalDown/2): #Candidates has less than half the total amount of downvotes
+				if (array[x][1]-array[x][2])/members > (0.1/10): #Checks their score is high enough compared to the total votes from the last polls
 					print("\nMEMBER ELECTED : "+array[x][0]+"\nUpvotes :",array[x][1],"\nDownvotes :",array[x][2])
+				else:
+					print("\n"+array[x][0]+" didn't have a high enough vote proportion")
+			else:
+				print("\n"+array[x][0]+" has more than half of the total downvotes")
+		else:
+			print("\n"+array[x][0]+" has too many downvotes compared to upvotes")
+	else:
+		print("\n"+array[x][0]+" has less than average upvotes")
+exportArray()
 
