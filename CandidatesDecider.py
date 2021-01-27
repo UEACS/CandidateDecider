8022db3f596ba9e2dc7328038ed9f5ee6760ef35
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-01-27 15:15:38 +0000

    Started Work On V3 with GUI
    Basic tkinter gui used to display text that  goes to the console. The import funciton now calculates the total up votes and total down votes instead of this being done in inputArray to allow for easier debugging and better abstraction.

--- CandidatesDecider.py
+++ CandidatesDecider.py
@@ -9,6 +9,8 @@
 # Version:	   0.1
 #-------------------------------------------------------------------------------
 
+from tkinter import *
+
 def file_len(fname):
     i=-1
     with open(fname) as f:
@@ -30,9 +32,6 @@ def inputArray():
 	name = input("Enter name of candidate\nEnter 'import' to import saved array")
 	if name == "import":
 		array = importArray()
-		for x in range(len(array)):
-			totalUp += array[x][1]
-			totalDown += array[x][2]
 		return array
 	while name != "":
 		up = int(input("Input up-votes"))
@@ -50,6 +49,8 @@ def exportArray():
 	arrayFile.close()
 
 def importArray():
+	global totalUp
+	global totalDown
 	try:
 		arrayFile = open("candidates.txt","r")
 	except FileNotFoundError:
@@ -61,13 +62,18 @@ def importArray():
 		array[x][1] = int(array[x][1])
 		array[x][2] = int(array[x][2])
 	arrayFile.close()
+	for x in range(len(array)):
+		totalUp += array[x][1]
+		totalDown += array[x][2]
 	return array
 
-array = inputArray()
+#array = inputArray()
+array = importArray()
 candidateNo = len(array)
 votesPerMember = (totalUp+totalDown)/members
 averageUp = totalUp/candidateNo
 averageDown = totalDown/candidateNo
+text = ""
 print("Election Results:")
 for x in range(candidateNo):
 	if array[x][1] > averageUp: #Upvotes must be more than average
@@ -75,13 +81,29 @@ for x in range(candidateNo):
 			if array[x][2] < round(totalDown/2): #Candidates has less than half the total amount of downvotes
 				if array[x][1]-array[x][2] > members*2/(totalUp+totalDown): #Checks if score is high enough compared to voters. (The more members that vote, the more elected)
 					print("\n   MEMBER ELECTED : "+array[x][0]+"\n   Upvotes :",array[x][1],"\n   Downvotes :",array[x][2])
+					text = text + str("\n   MEMBER ELECTED : "+array[x][0]+"\n   Upvotes :"+str(array[x][1])+"\n   Downvotes :"+str(array[x][2]))
 				else:
 					print("\n   "+array[x][0]+" didn't have a high enough vote proportion (A score higher than",round((0.5+members*2/(totalUp+totalDown))*10)/10,"was needed but only a score of ",(array[x][1]-array[x][2]),"was gotten)")
+					text = text + str("\n   "+array[x][0]+" didn't have a high enough vote proportion (A score higher than",round((0.5+members*2/(totalUp+totalDown))*10)/10,"was needed but only a score of ",(array[x][1]-array[x][2]),"was gotten)")
 			else:
 				print("\n   "+array[x][0]+" has more than half of the total downvotes")
+				text = text + str("\n   "+array[x][0]+" has more than half of the total downvotes")
 		else:
 			print("\n   "+array[x][0]+" has too many downvotes compared to upvotes")
+			text = text + str("\n   "+array[x][0]+" has too many downvotes compared to upvotes")
 	else:
 		print("\n   "+array[x][0]+" has less than average upvotes (",round(averageUp*10)/10,")")
+		text = text + str("\n   "+array[x][0]+" has less than average upvotes ("+str(round(averageUp*10)/10)+")")
 exportArray()
 
+window=Tk()
+window.title("Candidate Decider V3")
+window.geometry("720x480")
+
+
+DisplayText=Text(window,width=90,height=28,wrap=WORD)
+DisplayText.grid(row=0,column=0)
+DisplayText.insert(END,text)
+DisplayText.config(state=DISABLED)
+
+window.mainloop()
