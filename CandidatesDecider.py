427dfbf4e1df89f54802c720fb89b803f623e6af
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-01-28 20:14:48 +0000

    Change to the algorithm and small GUI changes
    In order to get elected, the criteria for a candidate to haves fewer than half the total ownvotes has changed to fewer than a third so people more hated have less of a chance of getting elected. The UI now has headers for number and candidate for the table and the import option will automatically refresh the page.

--- CandidatesDecider.py
+++ CandidatesDecider.py
@@ -66,12 +66,13 @@ def importArray():
 	except FileNotFoundError:
 		print("There was an error importing the file. Probably an empty file")
 
-
 	totalUp = 0
 	totalDown = 0
 	for x in range(len(array)):
 		totalUp += array[x][1]
 		totalDown += array[x][2]
+
+	calculateResults()
 
 def calculateResults():
 	global resultsArray
@@ -87,7 +88,7 @@ def calculateResults():
 	for x in range(candidateNo):
 		if array[x][1] > averageUp: #Upvotes must be more than average
 			if array[x][1] > array[x][2]*2: #Must have at least 2X more upvotes than downvotes
-				if array[x][2] < round(totalDown/2): #Candidates has less than half the total amount of downvotes
+				if array[x][2] < round(totalDown/3): #Candidates has few than a third of the total amount of downvotes
 					minVoteProportion = round((0.5+members*3/(totalUp+totalDown))*10)/10
 					if array[x][1]-array[x][2] > minVoteProportion: #Checks if score is high enough compared to voters. (The more members that vote, the more elected)
 						print("\n   MEMBER ELECTED : "+array[x][0]+"\n   Upvotes :",array[x][1],"\n   Downvotes :",array[x][2])
@@ -96,11 +97,11 @@ def calculateResults():
 						print("\n   "+array[x][0]+" didn't have a high enough vote proportion (A score higher than",minVoteProportion,"was needed but only a score of ",(array[x][1]-array[x][2]),"was gotten)")
 						resultsArray.append(str(array[x][0]+" didn't have a high enough vote proportion (A score higher than "+str(minVoteProportion)+" was needed but only a score of "+str(array[x][1]-array[x][2])+" was gotten)"))
 				else:
-					print("\n   "+array[x][0]+" has hlaf or more than the total downvotes")
-					resultsArray.append(str(array[x][0]+" has more than half of the total downvotes"))
+					print("\n   "+str(array[x][0]+" has "+str(array[x][2])+" downvotes, more than a third of the total downvotes ("+str(round((totalDown/3)*10)/10)+")"))
+					resultsArray.append(str(array[x][0]+" has "+str(array[x][2])+" downvotes, more than a third of the total downvotes ("+str(round((totalDown/3)*10)/10)+")"))
 			else:
-				print("\n   "+array[x][0]+" has too many downvotes compared to upvotes")
-				resultsArray.append(str(array[x][0]+" has too many downvotes compared to upvotes"))
+				print("\n   "+array[x][0]+" has too many downvotes "+str(array[x][2])+" compared to upvotes "+str(array[x][1]))
+				resultsArray.append(array[x][0]+" has too many downvotes "+str(array[x][2])+" compared to upvotes "+str(array[x][1]))
 		else:
 			print("\n   "+array[x][0]+" has fewer than average upvotes (",round(averageUp*10)/10,")")
 			resultsArray.append(str(array[x][0]+" has fewer than average upvotes ("+str(round(averageUp*10)/10)+")"))
@@ -122,14 +123,14 @@ def refreshResults():
 	for x in range(len(resultsArray)):
 		# Numbered colum on the left
 		tempTextDisplay = t.Text(selectionFrame,width=3,height=3,wrap=t.WORD,bd=4,bg="#36393f",fg="#87898C",highlightcolor="PURPLE",padx=4,pady=4,font=("Helvetica",15),relief=t.FLAT)
-		tempTextDisplay.grid(row = x,column=0,sticky=t.E)
+		tempTextDisplay.grid(row = x+1,column=0,sticky=t.E)
 		tempTextDisplay.insert(t.END,"\n  "+str(x+1)+":")
 		tempTextDisplay.config(state=t.DISABLED)
 		displayTextArray.append(tempTextDisplay)
 
 		# Candidate information
-		tempTextDisplay = t.Text(selectionFrame,width=40,height=3,wrap=t.WORD,bd=4,bg="GREY",fg="WHITE",highlightcolor="PURPLE",padx=4,font=("Helvetica",14),relief=t.FLAT)
-		tempTextDisplay.grid(row = x,column=1,sticky=t.W)
+		tempTextDisplay = t.Text(selectionFrame,width=40,height=3,wrap=t.WORD,bd=4,bg="#6B768C",fg="WHITE",highlightcolor="PURPLE",padx=4,font=("Helvetica",14),relief=t.FLAT)
+		tempTextDisplay.grid(row = x+1,column=1,sticky=t.W)
 		tempTextDisplay.insert(t.END,resultsArray[x])
 		tempTextDisplay.config(state=t.DISABLED)
 		displayTextArray.append(tempTextDisplay)
@@ -139,7 +140,7 @@ def refreshResults():
 
 def displayAboutWindow():
 	aboutWindow=t.Tk()
-	aboutWindow.title("About CD3")
+	aboutWindow.title("About CD 3.1")
 	aboutWindow.geometry("250x120")
 	aboutWindow.config(bg="#81A2E6")
 	aboutWindow.grid_anchor(t.N)
@@ -159,15 +160,15 @@ width = "1600"
 height = "760"
 
 window=t.Tk()
-window.title("Candidate Decider V3")
+window.title("Candidate Decider V3.1")
 window.geometry(width+"x"+height)
 window.config(bg="#403D37")
 window.grid_anchor(t.N)
 
 # Container or the votes
-mainFrame = t.Frame(window,bg="#403D37")
+mainFrame = t.Frame(window,bg="BROWN",width=200,cursor="dotbox")
 mainFrame.pack(fill=t.BOTH,expand=1)
-canvas = t.Canvas(mainFrame,bg="#36393f")
+canvas = t.Canvas(mainFrame,bg="#36393f",width=200)
 canvas.pack(side=t.LEFT,fill=t.BOTH,expand=1)
 # Scroll area
 scrollBar = t.Scrollbar(canvas,orient=t.VERTICAL,command=canvas.yview)
@@ -175,9 +176,19 @@ scrollBar.pack(side=t.RIGHT,fill=t.Y)
 canvas.configure(yscrollcommand=scrollBar.set)
 canvas.bind("<Configure>",lambda e:canvas.configure(scrollregion = canvas.bbox("all")))
 
-selectionFrame = t.Frame(canvas)
+selectionFrame = t.Frame(canvas,bg="#36393f")
 selectionFrame.grid_anchor(t.NE)
 canvas.create_window((0,0),window=selectionFrame,anchor="nw")
+
+numberHeader = t.Text(selectionFrame,width=3,height=1,wrap=t.WORD,bd=4,bg="#36393f",fg="#87898C",highlightcolor="PURPLE",padx=4,pady=8,font=("Helvetica",15),relief=t.FLAT)
+numberHeader.grid(row = 0,column=0,sticky=t.E)
+numberHeader.insert(t.END,"No.")
+numberHeader.config(state=t.DISABLED)
+
+candidateHeader = t.Text(selectionFrame,width=40,height=1,wrap=t.WORD,bd=4,bg="#6B768C",fg="WHITE",highlightcolor="PURPLE",padx=4,pady=8,font=("Helvetica",14),relief=t.FLAT)
+candidateHeader.grid(row = 0,column=1,sticky=t.W)
+candidateHeader.insert(t.END,"Candidate")
+candidateHeader.config(state=t.DISABLED)
 
 
 MenuBar=t.Menu(window)
